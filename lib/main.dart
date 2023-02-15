abc270c6fd42654c2a1554d9c6024aa9cd53ddde
Bruno Freitas <bruno.araujo@madeinweb.app>
2023-02-15 12:43:47 -0300

    feat: add image in home

--- lib/main.dart
+++ lib/main.dart
@@ -10,11 +10,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Pokedex',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Pokedex'),
     );
   }
 }
@@ -29,17 +29,39 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  var colorBackground = const Color(0XFF2c3151);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: colorBackground,
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          children: [
+            Container(
+              padding: const EdgeInsetsDirectional.fromSTEB(20, 20, 0, 0),
+              alignment: Alignment.topLeft,
+              child: const Text(
+                "Welcome to",
+                style: TextStyle(color: Colors.white),
+              ),
             ),
+            Container(
+              padding: const EdgeInsetsDirectional.fromSTEB(20, 20, 0, 0),
+              alignment: Alignment.topLeft,
+              child: const Text(
+                "Pokedex",
+                style: TextStyle(
+                  color: Color(0XFFf2bc11),
+                  fontWeight: FontWeight.bold,
+                  fontSize: 30
+                ),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsetsDirectional.fromSTEB(0, 30, 0, 0),
+              child: Image.asset('imagens/home_image.png'),
+            )
           ],
         ),
       ),
