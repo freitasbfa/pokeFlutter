1e5609a9d4b6461924633a8b1aecc2b2cd8c89bf
Bruno Freitas <bruno.araujo@madeinweb.app>
2023-02-15 17:03:01 -0300

    feat: organizing folders

---
+++ lib/screen/welcome.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:poke_flutter/screen/list_characters.dart';
+
+class WelcomePage extends StatefulWidget {
+  const WelcomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<WelcomePage> createState() => _WelcomePage();
+}
+
+class _WelcomePage extends State<WelcomePage> {
+  var colorBackground = const Color(0XFF2c3151);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: colorBackground,
+      body: Center(
+        child: Column(
+          children: [
+            Container(
+              padding: const EdgeInsetsDirectional.fromSTEB(20, 20, 0, 0),
+              alignment: Alignment.topLeft,
+              child: const Text(
+                "Welcome to",
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsetsDirectional.fromSTEB(20, 20, 0, 0),
+              alignment: Alignment.topLeft,
+              child: const Text(
+                "Pokedex",
+                style: TextStyle(
+                    color: Color(0XFFf2bc11),
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsetsDirectional.fromSTEB(0, 30, 0, 30),
+              child: Image.asset('imagens/image_home.png'),
+            ),
+            MaterialButton(
+              onPressed: () {
+                setState(() {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => ListCharactersPage()),
+                  );
+                });
+              },
+              minWidth: 450,
+              color: const Color(0XFFf2bc11),
+              child: const Text(
+                "Create Account",
+                style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsetsDirectional.fromSTEB(0, 20, 0, 0),
+              alignment: Alignment.center,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Text(
+                    "Already have an account?",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  SizedBox(
+                    width: 5,
+                  ),
+                  Text(
+                    "Login",
+                    style: TextStyle(
+                      color: Color(0XFFf2bc11),
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
